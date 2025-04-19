8ea17e86ce355b5b6273aa52d1b0032d7927b547
Suhana Chinchole <suhanachinchole@gmail.com>
2025-04-19 16:55:41 +0530

    Update main.tf with sg

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
     #!/bin/bash
