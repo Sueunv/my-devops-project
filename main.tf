45c8b8b27805ea4f785e763e8e2281b7eab7fa5d
Suhana Chinchole <suhanachinchole@gmail.com>
2025-04-19 16:53:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,36 @@ provider "aws" {
   region = var.region
 }
 
+
+# SECURITY GROUP to allow HTTP & SSH
+resource "aws_security_group" "web_sg" {
+  name        = "allow_http_ssh"
+  description = "Allow HTTP and SSH inbound traffic"
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
